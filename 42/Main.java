8dff0e9e52f30cf2ecf0c0c61151a01be64b9cb4
shawbrnz <shawbr@whs.school.nz>
2022-05-27 15:12:35 +1200

    Fixed the bug and added a varible to the size of the grid (bugged)

--- 42/Main.java
+++ 42/Main.java
@@ -2,7 +2,7 @@
  * Primary class
  *
  * @Brendan Shaw
- * @version 8, 20/5/22
+ * @version 9, 27/5/22
  */
 import java.io.File;//Allows file stuff
 import java.io.FileWriter;//Allows the writing of files so saved.
@@ -11,48 +11,10 @@ import java.util.Scanner;//Importing Scanner so keyboard inputs can be recorded.
 public class Main
 {
     // instance variables - replace the example below with your own
-    public boolean[][] grid = 
-        {{false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true}};
-    public boolean[][] lastGrid = 
-        {{false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false},
-            {true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true}};
+    final int xSize=20;
+    final int ySize=50;
+    public boolean[][] grid = new boolean[ySize][xSize];
+    public boolean[][] lastGrid = new boolean[ySize][xSize];
     final String SWAP_COMMAND="swap";
     final String END_COMMAND="end";
     final String START_COMMAND="start";
@@ -78,6 +40,8 @@ public class Main
             //Loops if active
             boolean active=false;
             boolean generation=false;
+            //Swap mode
+            boolean swapMode = false;
             //If true, then it wont render at the end of this loop. Used for the generation loop so 
             boolean dontPrint=false;
             //Do generations command
@@ -121,18 +85,19 @@ public class Main
             //swap command
             if ((scannerOutput.equals(SWAP_COMMAND))){
                 validCommand=true;
-                System.out.println("What do you wish to swap?");
-                try{
-                    String coordinates[]=scanner.nextLine().split(" ");//
-                    if(grid[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])]){
-                        grid[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])]=false;
+                swapMode=true;
+                dontPrint=true;
+                while(swapMode){
+                    System.out.println("What do you wish to swap? (Type '"+SWAP_COMMAND+"' to exit)");
+                    String cellSwapping[]=scanner.nextLine().split(" ");
+                    if(cellSwapping[0].equals(SWAP_COMMAND)){
+                        swapMode=false;
+                    }else if(cellSwapping[0].equals(END_COMMAND)){
+                        running=false;
+                        swapMode=false;
                     }else{
-                        grid[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])]=true;
+                        swap(cellSwapping);
                     }
-                }
-                catch(Exception e){
-                    //e.printStackTrace();
-                    System.out.println("Thats outside of this world!");
                 }
             }
 
@@ -176,9 +141,26 @@ public class Main
         }
     }
 
+    public void swap(String coordinates[]){
+        try{ 
+            if(grid[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])]){
+                grid[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])]=false;
+                lastGrid[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])]=false;
+            }else{
+                grid[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])]=true;
+                lastGrid[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])]=true;
+            }
+        }
+        catch(Exception e){
+            //e.printStackTrace();
+            System.out.println("Thats outside of this world!");
+        }
+        renderArray(grid);
+    }
+
     public void doGen(){
 
-        int[][] test={{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+        int[][] test=new int[xSize][xSize];
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[i].length;j++) {
                 test[i][j]=testCell(i,j);
@@ -187,7 +169,6 @@ public class Main
         gen++;
         System.out.println("Generation "+gen);
         renderArray(grid);
-        ///*
         System.out.println("Pot for "+gen);
         for(int i=0;i<test.length;i++){//Shows how many alive tiles adjacent
             String line="";
@@ -200,31 +181,37 @@ public class Main
             }
             System.out.println(line);
         }
-        System.out.println("End of pot for "+gen);//*/
-        lastGrid=grid;
+        System.out.println("End of pot for "+gen);
+        for(int i=0;i<grid.length;i++){//Converts last grid to this grid
+            for(int j=0;j<grid[i].length;j++) {                  
+                lastGrid[i][j]=grid[i][j];
+            }
+        }
     }
+
     public int testCell(int y, int x){
-                int nextToCells=0;
-                for(int i=-1;i<2;i++){
-                    for(int j=-1;j<2;j++){
-                        nextToCells+=testAdCell(i,j,y,x);
-                    }
-                }
-                if(nextToCells==3){//If the cell has three neighbors alive, then the cell is alive
-                    grid[y][x]=true;
-                }else if(nextToCells==2){//If the cell was alive and has two neighbors alive, then the cell is alive
-                    grid[y][x]=lastGrid[y][x];
-                }else{//Else the cell is dead
-                    grid[y][x]=false;
-                }
-                //lastGrid[y][x]=grid[y][x];
-                return nextToCells;//Returns alive adjecent cells for the array that shows alive adjecent cells
+        int nextToCells=0;
+        for(int i=-1;i<2;i++){
+            for(int j=-1;j<2;j++){
+                nextToCells+=testAdCell(i,j,y,x);
+            }
+        }
+        if(nextToCells==3){//If the cell has three neighbors alive, then the cell is alive
+            grid[y][x]=true;
+        }else if(nextToCells==2){//If the cell was alive and has two neighbors alive, then the cell is alive
+            grid[y][x]=lastGrid[y][x];
+        }else{//Else the cell is dead
+            grid[y][x]=false;
+        }
+        //lastGrid[y][x]=grid[y][x];
+        return nextToCells;//Returns alive adjecent cells for the array that shows alive adjecent cells
     }
+
     public int testAdCell(int yLook, int xLook, int yCurrent, int xCurrent){
-        if((yLook+yCurrent)<0){yLook=19;}
-        if((yLook+yCurrent)>19){yLook=-19;}
-        if((xLook+xCurrent)<0){xLook=19;}
-        if((xLook+xCurrent)>19){xLook=-19;}
+        if((yLook+yCurrent)<0){yLook=(ySize-1);}
+        if((yLook+yCurrent)>=(ySize)){yLook=0-(ySize-2);}
+        if((xLook+xCurrent)<0){xLook=(xSize-1);}
+        if((xLook+xCurrent)>=(xSize)){xLook=0-(xSize-2);}
         if (yLook==0&&xLook==0){//If it is testing the center cell, return 0
             return 0;}
         else if (lastGrid[yLook+yCurrent][xLook+xCurrent]){//If the tested cell in the last generation was alive, return 1
@@ -232,6 +219,6 @@ public class Main
         } else {//Else, return 0
             return 0;
         }
-        
+
     }
 }
